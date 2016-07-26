e23f0800dd899b7ef2d2808df1719439c656ebb7
ridinginstyle00 <ridinginstyle00@gmail.com>
2016-07-26 04:19:19 -0500

    Added the 2 python files; Levels and duels

---
+++ Levels/Levels.py
@@ -0,0 +1,261 @@
+import discord
+from discord.ext import commands
+from .utils import checks
+from .utils.dataIO import fileIO
+from __main__ import send_cmd_help
+from random import randint
+import os
+import time
+
+client = discord.Client()
+
+class Levels:
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.cooldown = 60
+        self.leader_board = fileIO("data/levels/leader_board.json", "load")
+        self.gettingxp = {}
+        self.xp_gaining_channels = []
+
+    @commands.group(name="rank", pass_context=True)
+    async def _rank(self, ctx):
+        """Rank operations"""
+        if ctx.invoked_subcommand is None:
+            await send_cmd_help(ctx)
+
+    @_rank.command(pass_context=True, no_pm=True)
+    async def join(self, ctx, user: discord.Member=None):
+        """Join ranking system"""
+        if not user:
+            user = ctx.message.author	
+            if user.id not in self.leader_board:
+                self.leader_board[user.id] = {"name": user.name, "rank": 0, "XP": 0}
+                fileIO("data/levels/leader_board.json", "save", self.leader_board)
+                await self.bot.say("{} has joined the Levelboard!".format(user.mention))
+            else:
+                await self.bot.say("{} has already joined and is rank {}".format(user.mention, str(self.get_rank(user.id))))
+        else:
+            await self.bot.say("Thanks to idiots overusing the ability to `rank join` others, this part of the command cannot be used! \nPlease get a Moderator or Admin to use `rank joino`!\nIf you're trying to `rank join` yourself...then please do `rank join` without a mention!")
+	
+    @_rank.command(name="joino", pass_context=True, no_pm=True)
+    @checks.admin_or_permissions(manage_server=True)
+    async def joino(self, ctx, user : discord.Member):
+        """Forces another person to join the ranking system"""
+        if not user:
+            if user.id not in self.leader_board:
+                self.leader_board[user.id] = {"name": user.name, "rank": 0, "XP": 0}
+                fileIO("data/levels/leader_board.json", "save", self.leader_board)
+                await self.bot.say("{} has joined the Levelboard!".format(user.mention))
+            else:
+                    await self.bot.say("{} has already joined and is rank SHUDDUP STUPID BOT {}".format(user.mention, str(self.get_rank(user.id))))
+        else:
+            if user.id not in self.leader_board:
+                self.leader_board[user.id] = {"name": user.name, "rank": 0, "XP": 0}
+                fileIO("data/levels/leader_board.json", "save", self.leader_board)
+                await self.bot.say("{} has joined the Levelboard!".format(user.mention))
+            else:
+                await self.bot.say("{} has already joined and is rank {}".format(user.mention, str(self.get_rank(user.id))))			
+
+    @_rank.command(name="set", pass_context=True, no_pm=True)
+    @checks.admin_or_permissions(manage_server=True)
+    async def _set(self, ctx, user : discord.Member, rank : int, xp : int):
+        """Set Rank and EXP"""
+        if user.id in self.leader_board:
+            self.leader_board[user.id] = {"name": user.name, "rank": rank, "XP": xp}
+            fileIO("data/levels/leader_board.json", "save", self.leader_board)
+            await self.bot.say("{}'s current stats are now: **Rank: {} XP {}/{}**".format(user.mention, self.get_rank(user.id),
+                                                                         self.get_xp(user.id),
+                                                                         self.get_level_xp(int(self.leader_board[user.id]["rank"]))))
+        else:
+            await self.bot.say("{} is not in the levelboard. Please make {} do `{}rank join` so their XP and Rank can be set!".format(user.mention, user.mention, ctx.prefix))
+			
+    @_rank.command(pass_context=True, no_pm=True)
+    async def leave(self, ctx, user : discord.Member=None):
+        """Resets your rank and EXP! UNDOABLE!!!"""
+        user = ctx.message.author
+        if user.id in self.leader_board:
+            del self.leader_board[user.id]
+            fileIO("data/levels/leader_board.json", "save", self.leader_board)
+            await self.bot.say("{} has left in the Levelboard!".format(user.mention))
+        else:
+            await self.bot.say("{} has not yet joined in the Levelboard! Do `{}rank join`!".format(user.mention, ctx.prefix))
+
+    @_rank.command(pass_context=True, no_pm=True)
+    @checks.admin_or_permissions(manage_server=True)	
+    async def leaveo (self, ctx, user : discord.Member):
+        """Forces another person to leave"""
+        if user.id in self.leader_board:
+            del self.leader_board[user.id]
+            fileIO("data/levels/leader_board.json", "save", self.leader_board)
+            await self.bot.say("{} has been removed from the Levelboard!".format(user.mention))
+        else:
+            await self.bot.say("{} has not yet joined in the Levelboard! Do `{}rank joino` to force them to join!".format(user.mention, ctx.prefix))
+
+    @_rank.command(name="show", pass_context=True)
+    async def _show(self, ctx, user : discord.Member=None):
+        """Show rank and XP of users.
+
+        Defaults to yours."""
+        if not user:
+            user = ctx.message.author  # LEVEL 13 | XP 1438/1595 | TOTAL XP 9888 | Rank 5/829
+            if self.check_joined(user.id):
+                await self.bot.say("{} **LEVEL {} | XP {}/{} **".format(user.name, self.get_rank(user.id),
+                                                                         self.get_xp(user.id),
+                                                                         self.get_level_xp(int(self.leader_board[user.id]["rank"]))))
+            else:
+                await self.bot.say("{} you are not in the ranking system. Type `{}rank join` to join".format(user.mention, ctx.prefix))
+        else:
+            if self.check_joined(user.id):
+                rank = self.get_rank(user.id)
+                xp = self.get_xp(user.id)
+                await self.bot.say("{}'s stats: **LEVEL {} | XP {}/{} **".format(user.mention, self.get_rank(user.id),
+                                                                         self.get_xp(user.id),
+                                                                         self.get_level_xp(int(self.leader_board[user.id]["rank"]))))
+            else:
+                await self.bot.say("This user has not joined the rank system")
+
+    @_rank.command(pass_context=True)
+    async def levelup(self, ctx):
+        """level up. Mainly used in case Auto-Leveling doesn't work!"""
+        user = ctx.message.author
+        if self.leader_board[user.id]["XP"] >= self.get_level_xp(self.leader_board[user.id]["rank"]):
+            self.leader_board[user.id]["rank"] += 1
+            self.leader_board[user.id]["XP"] -= self.get_level_xp(self.leader_board[user.id]["rank"])
+            await self.bot.say("{}: Level Up! You are now level {}".format(user.mention,
+                                                                           self.leader_board[user.id]["rank"]))
+        else:
+            await self.bot.say("You are not ready to level up yet {}".format(user.mention))
+			
+    @_rank.command(pass_context=True, no_pm=True)
+    async def creators(self, ctx):
+        """Shows the creators of the Rank cog!"""
+        user = ctx.message.author
+        await self.bot.say("```There were hard efforts in the making of this cog.\n \n It took LOTS of time to make a ranking system for RED, that could replace MEE6's ranking system \n \n Here is the list of creators for this cog: \n \n funnyman \n \n Ridinginstyle00 \n \n We appreciate the help from others that helped us!!```")
+
+    @commands.group(name="cooldown", pass_context=True)
+    async def _cooldown(self, ctx):
+        """cooldown options"""
+        if ctx.invoked_subcommand is None:
+            await send_cmd_help(ctx)
+
+    @_cooldown.command(pass_contex=True)
+    @checks.is_owner()
+    async def set(self, cooldown: int=60):
+        """sets XP cool down interval"""
+        self.cooldown = cooldown
+
+    @_rank.command()
+    async def levelboard(self, top: int=10):
+        """Prints out the rank leaderboard
+        Defaults to top 10"""  # Originally coded by Airenkun - edited by irdumb - edited for ranking by Funnyman2213
+        if top < 1:
+            top = 10
+        if top > 20:
+            top = 20
+        leader_board_sorted = sorted(self.leader_board.items(), key=lambda x: x[1]["rank"], reverse=True)
+        if len(leader_board_sorted) < top:
+            top = len(leader_board_sorted)
+        topten = leader_board_sorted[:top]
+        highscore = ""
+        place = 1
+        for id in topten:
+            highscore += str(place).ljust(len(str(top)) + 1)
+            highscore += (id[1]["name"] + " ").ljust(23 - len(str(id[1]["rank"])))
+            highscore += str(id[1]["rank"]) + "\n"
+            place += 1
+        if highscore:
+            if len(highscore) < 1985:
+                await self.bot.say("```py\n" + highscore + "```")
+            else:
+                await self.bot.say("The leaderboard is too big to be displayed. Try with a lower <top> parameter.")
+        else:
+            await self.bot.say("No one has joined the rank system.")
+			
+    async def gain_xp(self, message):
+        user = message.author
+        id = user.id
+        if self.check_joined(id):
+            if id in self.gettingxp:
+                seconds = abs(self.gettingxp[id] - int(time.perf_counter()))
+                if seconds >= self.cooldown:
+                    self.add_xp(id)
+                    self.gettingxp[id] = int(time.perf_counter())
+                    fileIO("data/levels/leader_board.json", "save", self.leader_board)
+                if self.leader_board[user.id]["XP"] >= self.get_level_xp(self.leader_board[user.id]["rank"]):
+                    self.leader_board[user.id]["rank"] += 1
+                    self.leader_board[user.id]["XP"] = 0
+                    msg = '{} **has leveled up and is now level {}!!!\n HURRAY!!**'
+                    msg = msg.format(message.author.mention, self.leader_board[user.id]["rank"])
+                    await self.bot.send_message(message.channel, msg)
+                    fileIO("data/levels/leader_board.json", "save", self.leader_board)
+            else:
+                self.add_xp(id)
+                self.gettingxp[id] = int(time.perf_counter())
+                fileIO("data/levels/leader_board.json", "save", self.leader_board)
+
+    def add_xp(self, id):
+        if self.check_joined(id):
+            self.leader_board[id]["XP"] += int(randint(15, 20))
+
+    def mention_from_id(self, id):
+        return "@" + str(self.leader_board[id]["name"]) + "#" + str(id)
+
+    def get_level_xp(self, level):
+        xp = 5*(int(level)**2)+50*int(level)+100
+        return xp
+
+    def check_joined(self, id):
+        if id in self.leader_board:
+            return True
+        else:
+            return False
+
+    def get_rank(self, id):
+        if self.check_joined(id):
+            return self.leader_board[id]["rank"]
+
+    def get_xp(self, id):
+        if self.check_joined(id):
+            return self.leader_board[id]["XP"]
+
+    def display_time(self, seconds, granularity=2):  # What would I ever do without stackoverflow?
+        intervals = (  # Source: http://stackoverflow.com/a/24542445
+            ('weeks', 604800),  # 60 * 60 * 24 * 7
+            ('days', 86400),  # 60 * 60 * 24
+            ('hours', 3600),  # 60 * 60
+            ('minutes', 60),
+            ('seconds', 1),
+        )
+
+        result = []
+
+        for name, count in intervals:
+            value = seconds // count
+            if value:
+                seconds -= value * count
+                if value == 1:
+                    name = name.rstrip('s')
+                result.append("{} {}".format(value, name))
+        return ', '.join(result[:granularity])
+
+
+def check_folders():
+    if not os.path.exists("data/levels"):
+        print("Creating data/levels folder...")
+        os.mkdir("data/levels")
+
+
+def check_files():
+    fp = "data/levels/leader_board.json"
+    if not fileIO(fp, "check"):
+        print("Creating empty Leader board...")
+        fileIO(fp, "save", {})
+
+
+def setup(bot):
+    check_folders()
+    check_files()
+    n = Levels(bot)
+    bot.add_listener(n.gain_xp, "on_message")
+    bot.add_cog(n)
---
+++ duels/duels.py
@@ -0,0 +1,314 @@
+import discord
+from discord.ext import commands
+from .utils import checks
+from .utils.dataIO import dataIO
+from __main__ import send_cmd_help
+from __main__ import settings
+from random import choice
+from random import sample
+import os
+import logging
+import aiohttp
+import asyncio
+import time
+import copy
+
+
+client = discord.Client()
+
+class Duels:
+
+	def __init__(self, bot):
+		self.bot = bot
+		self.duelist = dataIO.load_json("data/duels/duelist.json")
+		self.nuels = "duels"
+		self.wlt = dataIO.load_json("data/duels/account.json")
+		
+	@commands.group(name="duels", pass_context=True)
+	async def _duels(self, ctx):
+		"""Duel with another player!!"""
+		if ctx.invoked_subcommand is None:
+			await send_cmd_help(ctx)
+	
+	@commands.command(name="duel", pass_context=True, no_pm=True)
+	async def _duel(self, ctx, user: discord.Member, otheruser : discord.Member):
+		"""Duel another player"""
+		author = ctx.message.author
+		if not user:
+			await self.bot.say("{} please mention a user to duel with!".format(author.mention))
+		else:
+						try:
+							nick_player1 = user.name
+							nick_player2 = otheruser.name
+							action = self.duelist[self.nuels]
+							action_damage1, action_damage2, action_damage3, action_damage4 = self.action_damage()
+							action_chosen1, action_chosen2, action_chosen3, action_chosen4 = sample(action,4)
+							hp_player1 = 30
+							hp_player2 = 30
+							player1_id = user.id
+							player2_id = otheruser.id
+				
+							await self.bot.say("**{}** dueled **{}**!!\n\nPlease wait for the duel to start! Both players will begin with **{}** health!".format(user.mention, otheruser.mention, hp_player1))
+							await asyncio.sleep(1)
+							await self.bot.say("**{}** `{}` **{}** and took off **{}** health!".format(nick_player1, action_chosen1, nick_player2, action_damage1))
+							hp_player2 = hp_player2 - action_damage1
+							await asyncio.sleep(1)
+							await self.bot.say("**{}** `{}` **{}** and took off **{}** health!".format(nick_player2, action_chosen2, nick_player1, action_damage2))
+							hp_player1 = hp_player1 - action_damage2
+							await asyncio.sleep(1)	
+							await self.bot.say("**{}** `{}` **{}** and took off **{}** health!".format(nick_player1, action_chosen3, nick_player2, action_damage3))
+							hp_player2 = hp_player2 - action_damage3
+							await asyncio.sleep(1)
+							await self.bot.say("**{}** `{}` **{}** and took off **{}** health!".format(nick_player2, action_chosen2, nick_player1, action_damage4))
+							hp_player1 = hp_player1 - action_damage4
+	
+							if hp_player1 > hp_player2:
+								winner_player = nick_player1
+								losing_player = nick_player2
+								remaining_hp = hp_player1
+								await asyncio.sleep(1)	
+								await self.bot.say("After 4 rounds of bloody combat, the winner is **{}** with **{}** health!".format(winner_player, remaining_hp))
+								if player1_id not in self.wlt:
+									self.wlt[player1_id] = {"name": author.name, "Wins": 0, "Losses": 0, "Ties": 0}
+									dataIO.save_json("data/duels/account.json", self.wlt)
+									await self.bot.say("{} has not yet entered the duel tournament!".format(winner_player))
+									await asyncio.sleep(.5)
+									await self.bot.say("{} has joined the duel tournament, currently changing settings!".format(winner_player))
+									await self.bot.say("{} gained +1 WIN!!".format(winner_player))
+									self.wlt[player1_id]["Wins"] += 1
+									dataIO.save_json("data/duels/account.json", self.wlt)
+								else:
+									await self.bot.say("{} gained +1 WIN!!".format(winner_player))
+									self.wlt[player1_id]["Wins"] += 1
+									dataIO.save_json("data/duels/account.json", self.wlt)
+		
+								if player2_id not in self.wlt:
+									self.wlt[player2_id] = {"name": user.name, "Wins": 0, "Losses": 0, "Ties": 0}
+									dataIO.save_json("data/duels/account.json", self.wlt)
+									await self.bot.say("{} has not yet entered the duel tournament!".format(losing_player))
+									await asyncio.sleep(.5)
+									await self.bot.say("{} has joined the duel tournament, currently changing settings!".format(losing_player))
+									self.wlt[player2_id]["Losses"] += 1
+									dataIO.save_json("data/duels/account.json", self.wlt)
+								else:
+									await self.bot.say("{} gained +1 LOSE!!".format(losing_player))
+									self.wlt[player2_id]["Losses"] += 1
+									dataIO.save_json("data/duels/account.json", self.wlt)
+						
+							elif hp_player1 == hp_player2:
+								remaining_hp = hp_player1
+								await asyncio.sleep(1)	
+								await self.bot.say("After 4 rounds of bloody combat, the winner is **no one because it's a draw** with both players still having **{}** health!".format(remaining_hp))
+								if player1_id not in self.wlt:
+									self.wlt[player1_id] = {"name": author.name, "Wins": 0, "Losses": 0, "Ties": 0}
+									dataIO.save_json("data/duels/account.json", self.wlt)
+									await self.bot.say("{} has not yet entered the duel tournament!".format(nick_player1))
+									await asyncio.sleep(.5)
+									await self.bot.say("{} has joined the duel tournament, currently changing settings!".format(nick_player1))
+									await self.bot.say("{} gained +1 TIE!!".format(nick_player1))
+									self.wlt[player1_id]["Ties"] += 1
+									dataIO.save_json("data/duels/account.json", self.wlt)
+								else:
+									await self.bot.say("{} gained +1 TIE!!".format(nick_player1))
+									self.wlt[player1_id]["Ties"] += 1
+									dataIO.save_json("data/duels/account.json", self.wlt)
+								
+								if player2_id not in self.wlt:
+									self.wlt[player2_id] = {"name": user.name, "Wins": 0, "Losses": 0, "Ties": 0}
+									dataIO.save_json("data/duels/account.json", self.wlt)
+									await self.bot.say("{} has not yet entered the duel tournament!".format(nick_player2))
+									await asyncio.sleep(.5)
+									await self.bot.say("{} has joined the duel tournament, currently changing settings!".format(nick_player2))
+									await self.bot.say("{} gained +1 TIE!!".format(nick_player2))
+									self.wlt[player2_id]["Ties"] += 1
+									dataIO.save_json("data/duels/account.json", self.wlt)
+								else:
+									await self.bot.say("{} gained +1 TIE!!".format(nick_player2))
+									self.wlt[player2_id]["Ties"] += 1
+									dataIO.save_json("data/duels/account.json", self.wlt)
+								
+							else:
+								winner_player = nick_player2
+								losing_player = nick_player1
+								remaining_hp = hp_player2
+								await asyncio.sleep(1)	
+								await self.bot.say("After 4 rounds of bloody combat, the winner is **{}** with **{}** health!".format(winner_player, remaining_hp))
+								if player2_id not in self.wlt:
+									self.wlt[player2_id] = {"name": user.name, "Wins": 0, "Losses": 0, "Ties": 0}
+									dataIO.save_json("data/duels/account.json", self.wlt)
+									await self.bot.say("{} has not yet entered the duel tournament!".format(winner_player))
+									await asyncio.sleep(.5)
+									await self.bot.say("{} has joined the duel tournament, currently changing settings!".format(winner_player))
+									await self.bot.say("{} gained +1 WIN!!".format(winner_player))
+									self.wlt[player2_id]["Wins"] += 1
+									dataIO.save_json("data/duels/account.json", self.wlt)
+								else:
+									await self.bot.say("{} gained +1 WIN!!".format(winner_player))
+									self.wlt[player2_id]["Wins"] += 1
+									dataIO.save_json("data/duels/account.json", self.wlt)
+								if player1_id not in self.wlt:
+									self.wlt[player1_id] = {"name": author.name, "Wins": 0, "Losses": 0, "Ties": 0}
+									dataIO.save_json("data/duels/account.json", self.wlt)
+									await self.bot.say("{} has not yet entered the duel tournament!".format(losing_player))
+									await asyncio.sleep(.5)
+									await self.bot.say("{} has joined the duel tournament, currently changing settings!".format(losing_player))
+									await self.bot.say("{} gained +1 LOSE!!".format(losing_player))
+									self.wlt[player1_id]["Losses"] += 1
+									dataIO.save_json("data/duels/account.json", self.wlt)
+								else:
+									await self.bot.say("{} gained +1 LOSE!!".format(losing_player))
+									self.wlt[player1_id]["Losses"] += 1
+									dataIO.save_json("data/duels/account.json", self.wlt)
+							
+						except IndexError:
+							await self.bot.say("Uh oh! it seems your list is empty! Add options with %cduels add" % settings.prefixes[0])
+						except KeyError:
+							await self.bot.say("Uh oh! it seems your list is empty! Add options with %cduels add" % settings.prefixes[0])
+	@_duels.command(pass_context=True, no_pm=True)
+	@checks.admin_or_permissions(manage_server=True)
+	async def add (self, ctx, Duel : str):
+		"""Adds a duel to the list"""
+		if self.nuels not in self.duelist:
+			self.duelist[self.nuels] = []
+			dataIO.save_json("data/duels/duelist.json", self.duelist)
+		if Duel in self.duelist[self.nuels]:
+			await self.bot.say("Uh oh. It seems {} has already been added to the list.".format(Duel))
+		else:
+			self.duelist[self.nuels].append(Duel)
+			dataIO.save_json("data/duels/duelist.json", self.duelist)
+			await self.bot.say("{} has been added to the duel list!".format(Duel))
+			
+	@_duels.command(pass_context=True, no_pm=True)
+	async def join(self, ctx, user: discord.Member=None):
+		"""Join tournament"""
+		user = ctx.message.author	
+		if user.id not in self.wlt:
+			self.wlt[user.id] = {"name": user.name, "Wins": 0, "Losses": 0, "Ties": 0}
+			dataIO.save_json("data/duels/account.json", self.wlt)
+			await self.bot.say("{} has joined the tournament!".format(user.mention))
+		else:
+			await self.bot.say("{} has already joined the tournament".format(user.mention))
+			
+	@_duels.command(name="stats", pass_context=True)
+	async def _stats(self, ctx, user : discord.Member=None):
+		"""Show rank and XP of users.
+
+		Defaults to yours."""
+		if not user:
+			user = ctx.message.author 
+			if self.check_joined(user.id):
+				await self.bot.say("{}'s stats: **Wins: {} | Losses: {} | Ties: {} **".format(user.name, self.get_wins(user.id),
+                                                                         self.get_losses(user.id),
+                                                                         self.get_ties(user.id)))
+			else:
+				await self.bot.say("{}, you are not yet in the tournament!".format(user.mention))
+		else:
+			if self.check_joined(user.id):
+				await self.bot.say("{}'s stats: **Wins: {} | Losses: {} | Ties: {} **".format(user.name, self.get_wins(user.id),
+                                                                         self.get_losses(user.id),
+                                                                         self.get_ties(user.id)))
+			else:
+				await self.bot.say("This user has not joined the tournament")
+				
+	@_duels.command(pass_context=True, no_pm=True)
+	async def show (self, ctx):
+		"""Shows list of available duels"""
+		s = "\n"
+		y = s.join(self.duelist[self.nuels])
+		await self.bot.say("List of available duel things:\n{}".format(y))
+		
+	@_duels.command(pass_context=True, no_pm=True)
+	async def remove (self, ctx, Duel : str):
+		"""removes a duel from the list"""
+		try:
+			x = self.duelist[self.nuels].remove(Duel)
+			if x is not ValueError:
+				dataIO.save_json("data/duels/duelist.json", self.duelist)
+				await self.bot.say("{} has been successfully removed from the duel list!".format(Duel))
+		except ValueError:
+			await self.bot.say("I can't remove what hasn't been added to the list to begin with.")
+			
+	@_duels.command(pass_context=True, no_pm=True)
+	async def reset (self, ctx):
+		"""For when you have waaay too many duels"""
+		if len(self.duelist[self.nuels]) > 0:
+			self.duelist[self.nuels] = []
+			dataIO.save_json("data/duels/duelist.json", self.duelist)
+			await self.bot.say("Duel list has been reset")
+		else:
+			await self.bot.say("I can't delete a list that's already empty")
+	
+	def user_or_author (self, author, user):
+		player_list = [author, user]
+		player1 = choice(player_list)
+		if player1 == author:
+			player2 = user
+			return player1, player2
+		else:
+			player2 = author
+			return player1, player2
+			
+	def action_choose (self):
+		action = choice(sample(self.duelist[self.nuels],1))
+		return action
+
+	def multiple_action_choose (self):
+		action1 = self.action_choose()
+		action2 = self.action_choose()
+		action3 = self.action_choose()
+		action4 = self.action_choose()
+		return action1, action2, action3, action4
+		
+	def action_damage (self):
+		action_chosen1, action_chosen2, action_chosen3, action_chosen4 = self.multiple_action_choose()
+		action_damage1 = self.duelist[self.nuels].index(action_chosen1)
+		action_damage2 = self.duelist[self.nuels].index(action_chosen2)
+		action_damage3 = self.duelist[self.nuels].index(action_chosen3)
+		action_damage4 = self.duelist[self.nuels].index(action_chosen4)
+		return action_damage1, action_damage2, action_damage3, action_damage4
+		
+	def check_joined(self, id):
+		if id in self.wlt:
+			return True
+		else:
+			return False
+			
+	def get_wins(self, id):
+		if self.check_joined(id):
+			return self.wlt[id]["Wins"]
+
+	def get_losses(self, id):
+		if self.check_joined(id):
+			return self.wlt[id]["Losses"]
+			
+	def get_ties(self, id):
+		if self.check_joined(id):
+			return self.wlt[id]["Ties"]
+		
+def check_folders():
+    if not os.path.exists("data/duels"):
+        print("Creating data/duels folder...")
+        os.mkdir("data/duels")
+		
+def check_files():
+    fp = "data/duels/duelist.json"
+    if not dataIO.is_valid_json(fp):
+        print("Creating duelist.json...")
+        dataIO.save_json(fp, {})
+    wlt = "data/duels/account.json"
+    if not dataIO.is_valid_json(wlt):
+        print("creating account.json...")
+        dataIO.save_json(wlt, {})
+
+def setup(bot):
+    global logger
+    check_folders()
+    check_files()
+    n = Duels(bot)
+    logger = logging.getLogger("red.duels")
+    if logger.level == 0: # Prevents the logger from being loaded again in case of module reload
+        logger.setLevel(logging.INFO)
+        handler = logging.FileHandler(filename='data/duels/duels.log', encoding='utf-8', mode='a')
+        handler.setFormatter(logging.Formatter('%(asctime)s %(message)s', datefmt="[%d/%m/%Y %H:%M]"))
+        logger.addHandler(handler)
+    bot.add_cog(n)
